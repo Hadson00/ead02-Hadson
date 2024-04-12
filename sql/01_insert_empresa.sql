0d1a874e5ca8c8db0cdbd15a721422065601a95d
Hadson00 <hadson798@gmail.com>
2024-04-11 22:19:12 -0300

    a

---
+++ sql/01_insert_empresa.sql
@@ -0,0 +1,6 @@
+INSERT INTO `empresa`(`nome`, `cnpj`, `gestao`)
+VALUES ('Y', '12345678912132', 'Elon Moská');
+INSERT INTO `empresa`(`nome`, `cnpj`, `gestao`)
+VALUES ('Facelivro', '45271892234718', 'Mark Heisenberg');
+
+SELECT * FROM `empresa`;
---
+++ sql/02_insert_funcionario.sql
@@ -0,0 +1,8 @@
+INSERT INTO `funcionario`(`nome`, `endereco`, `CEP`, `CPF`, `telefone`, `idEmpresa`)
+VALUES('Raphael', 'Descoberto', '36690000', '18293962731', '32998564203', 1);
+INSERT INTO `funcionario`(`nomeFuncionario`, `endereco`, `CEP`, `CPF`, `telefone`, `idEmpresa`)
+VALUES('Rogério', 'Descoberto', 36690000, 97126437853, '32984759832', 2);
+INSERT INTO `funcionario`(`nomeFuncionario`, `endereco`, `CEP`, `CPF`, `telefone`, `idEmpresa`)
+VALUES('Camylle', 'Descoberto', 36690000, 80127480614, '32998384657', 2);
+
+SELECT * FROM `funcionario`;
